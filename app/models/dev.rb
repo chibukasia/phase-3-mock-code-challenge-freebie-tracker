687dabf0f4ab51e2225c8d2f7981b1c886fe0c01
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-08-26 19:20:32 +0300

    done

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,20 @@
 class Dev < ActiveRecord::Base
+    has_many (:freebies)
+    has_many :companies, through: :freebies
 
+    def received_one?(item_name)
+        self.class.find_by(item_name:item_name)
+        #**********#
+        # **TODO** #
+        #**********#
+    end
+
+
+    def give_away(dev, freebie)
+        if freebie.dev_id == self.id 
+            "TODO"
+        else
+            "TODO"
+        end
+    end
 end
