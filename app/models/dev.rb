e40aaacc786a010da6e7e4e91dc917bd44875484
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-08-29 18:49:53 +0300

    Done.

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -3,18 +3,14 @@ class Dev < ActiveRecord::Base
     has_many :companies, through: :freebies
 
     def received_one?(item_name)
-        self.class.find_by(item_name:item_name)
-        #**********#
-        # **TODO** #
-        #**********#
+        
+        self.freebies.any? do |freebie|
+            freebie.item_name == item_name
+        end
     end
 
 
     def give_away(dev, freebie)
-        if freebie.dev_id == self.id 
-            "TODO"
-        else
-            "TODO"
-        end
+        freebie.update(dev: dev) unless freebie.dev != self.id
     end
 end
